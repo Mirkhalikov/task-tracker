887bceed7a71efbce1e80ac57d80eca3c34e6938
Ярослав Мирхаликов <mirkh04@mail.ru>
2024-12-11 22:51:23 +0300

    SO BIG UPDATE: Стилизовал все существующие страницы (нужно будет ещё доделать некоторые моменты), добавил шапку на сайт

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,12 @@ class ApplicationController < ActionController::Base
 
   before_action :set_current_user
 
+  helper_method :current_user
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
   private
 
   def set_current_user
