c7789207f1e777cb53a1b5ec004b3b13795bac08
Ярослав Мирхаликов <mirkh04@mail.ru>
2024-12-11 21:38:33 +0300

    Некоторые добавления по пользователю (Планирую добавить аватары)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
     @user = User.new
   end
@@ -6,15 +14,28 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to root_path, notice: "Регистрация прошла успешно."
+      redirect_to root_path, notice: 'Регистрация прошла успешно.'
     else
       render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to users_path, notice: 'Профиль обновлён.'
+    else
+      render :edit
     end
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:username, :password_hash)
+    params.require(:user).permit(:username, :password_hash, :avatar)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
-  root "tasks#index"
+  root 'tasks#index'
 
-  get "/signup", to: "users#new"
-  post "/signup", to: "users#create"
-  get "/login", to: "sessions#new"
-  post "/login", to: "sessions#create"
-  delete "/logout", to: "sessions#destroy"
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+  resources :users do
+    member do
+      get :edit
+      patch :update
+    end
+  end
 
   resources :tasks do
     member do
