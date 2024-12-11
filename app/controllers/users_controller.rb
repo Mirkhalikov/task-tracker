618dd5f9ff8cefacb340709fb3dc28d7a6e7ae62
Ярослав Мирхаликов <mirkh04@mail.ru>
2024-12-11 22:00:53 +0300

    RubocopFix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to tasks_path, notice: 'Регистрация прошла успешно.'
+      redirect_to tasks_path, notice: "Регистрация прошла успешно."
     else
       render :new
     end
