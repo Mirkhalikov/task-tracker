26d0a25f78854b53e19c1eefafc3116bcc6a73a8
Ярослав Мирхаликов <mirkh04@mail.ru>
2024-12-11 21:29:03 +0300

    Описал логику контроллера задач

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,50 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+    @categories = Category.all
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.user = current_user
+    if @task.save
+      redirect_to tasks_path, notice: 'Задача создана.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+    @categories = Category.all
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to task_path(@task), notice: 'Задача обновлена.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path, notice: 'Задача удалена.'
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :description, :status, category_ids: [])
+  end
 end
